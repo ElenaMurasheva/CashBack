bab335c503a644a8dd6f15ad291dcf03ad63e8cf
Елена Мурашева <vacilika77@gmail.com>
2022-07-31 18:46:44 +0300

    Modified testng

---
+++ src/test/java/ru/netology/service/CashbackHackerServiceTest.java
@@ -0,0 +1,58 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+
+public class CashbackHackerServiceTest {
+
+    @Test
+    public void shouldAddIfSummLess1000() {
+
+        CashbackHackerService hackerService = new CashbackHackerService();
+
+        int amount = 450;
+        int actual = hackerService.remain(amount);
+        int expected = 550;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldAddiIfSumm0() {
+
+        CashbackHackerService hackerService = new CashbackHackerService();
+
+        int amount = 0;
+        int actual = hackerService.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldAddIfSummOver1000() {
+
+        CashbackHackerService hackerService = new CashbackHackerService();
+
+        int amount = 1350;
+        int actual = hackerService.remain(amount);
+        int expected = 650;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldAddIfSummOver5000() {
+
+        CashbackHackerService hackerService = new CashbackHackerService();
+
+        int amount = 5720;
+        int actual = hackerService.remain(amount);
+        int expected = 280;
+
+        assertEquals(actual, expected);
+    }
+
+}
