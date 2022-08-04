a8d2af50b3640396a78a4b5b00b35af76b154bdf
Елена Мурашева <vacilika77@gmail.com>
2022-08-04 15:26:41 +0300

    Added tests

--- src/test/java/ru/netology/service/CashbackHackerServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackerServiceTest.java
@@ -32,6 +32,18 @@ public class CashbackHackerServiceTest {
     }
 
     @Test
+    public void shouldAddiIfSumm1000() {
+
+        CashbackHackerService hackerService = new CashbackHackerService();
+
+        int amount = 1000;
+        int actual = hackerService.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
     public void shouldAddIfSummOver1000() {
 
         CashbackHackerService hackerService = new CashbackHackerService();
